8d4f898cb351558414952b21e8fbb47728121e03
Ayoub Afkir <106391858+ayoub63@users.noreply.github.com>
2024-03-28 19:26:24 +0100

    Implemented PLayer Choice Function

--- game.js
+++ game.js
@@ -1,8 +1,26 @@
-function getComputerChoice () {
-    const strings = ["Rock" , "Paper" , "Scissors"];
+function getComputerChoice() {
+  const strings = ["rock", "paper", "scissors"];
 
-    const randNum = Math.floor(Math.random() * 3);
-    
-    return strings [randNum];
+  const randNum = Math.floor(Math.random() * 3);
 
+  return strings[randNum];
 }
+
+function getPlayerChoice() {
+  finish = true;
+  while (finish) {
+    let userChoice = prompt("Pick Rock , Paper , or Scissors. ").toLowerCase();
+
+    if (
+      userChoice == "rock" ||
+      userChoice == "paper" ||
+      userChoice == "scissors"
+    ) {
+      finish = false;
+    } else {
+      alert("Invalid input. Please enter 'rock', 'paper', or 'scissors'.");
+    }
+  }
+  return userChoice;
+}
+
