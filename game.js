1ffa5b707aada498f59a9d73ee0bc5120692a8fe
Ayoub Afkir <106391858+ayoub63@users.noreply.github.com>
2024-03-30 18:09:00 +0100

    removed game function

--- game.js
+++ game.js
@@ -29,11 +29,11 @@ function playRound() {
   const getComputerChoice1 = getComputerChoice();
   const getPlayerChoice2 = getPlayerChoice();
   let compWin1 = "Computer wins, Computer picked " + getComputerChoice1;
-    let playWin2 = "Player wins,  Computer picked " + getComputerChoice1;
+  let playWin2 = "Player wins,  Computer picked " + getComputerChoice1;
   if (getComputerChoice1 == getPlayerChoice2) {
     return "It is a tie!";
   } else if (
-    (getComputerChoice1== "rock" && getPlayerChoice2 == "paper") ||
+    (getComputerChoice1 == "rock" && getPlayerChoice2 == "paper") ||
     (getComputerChoice1 == "paper" && getPlayerChoice2 == "rock") ||
     (getComputerChoice1 == "scissors" && getPlayerChoice2 == "paper")
   ) {
@@ -41,33 +41,4 @@ function playRound() {
   } else {
     return playWin2;
   }
-}
- 
-
-
-function playGame () {
-    let compWin = "Computer wins";
-    let playWin = "Player wins";
-    countPlayer = 0;
-    countCom = 0;
-    for (i = 0 ; i < 5 ; i++) {
-       let roundResult = playRound();
-        if (roundResult.startsWith(compWin)) {
-            countCom++;
-        }
-        else if(roundResult.startsWith(playWin)) {
-            countPlayer++;
-        }}
-        if (countPlayer > countCom) {
-            return "Player won over 5 rounds " + "Computer Score: " + countCom + "Player Score: " + countPlayer ;
-        }
-        else 
-        if (countCom > countPlayer) {
-            return "Computer won over 5 rounds " + "Computer Score: " + countCom + " Player Score: " + countPlayer ;
-        }
-       
-     return "No winner after 5 rounds. Computer Score: " + countCom + ", Player Score: " + countPlayer;
-    
-    
-
 }
